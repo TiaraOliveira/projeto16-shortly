24c1e8fdadff226faa5bc89b4f58ec1056186ddd
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-08-08 18:22:02 -0300

    fix ranking

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -4,9 +4,13 @@ import connection from '../dbStrategy/postgres.js';
 export async function ranking(req, res){
  
    try {
-      const {rows: ranking} = await connection.query(' SELECT users.id, users.name AS name, count(urls.id) as linksCount, SUM(urls."visitCount") as "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id LIMIT 10;')
+      const {rows: ranking} = await connection.query(`
+      SELECT users.id, users.name AS name, 
+count(urls.id) as linksCount, SUM(urls."visitCount") as "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" 
+GROUP BY users.id ORDER BY "visitCount" LIMIT 10;`)
       res.status(200).send(ranking);
    } catch (error) {
     res.status(500).send(error); 
    }
+   
 }
