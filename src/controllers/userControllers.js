5e6acc2f3b36f1169fb3028f2cb539db1b60f5c9
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-08-08 14:41:29 -0300

    getusersUrls

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 
 export async function getUserUrls(req, res){
     const token = req.headers.authorization.split(' ')[1]
+    console.log(token)
     const secretKey = process.env.JWT_SECRET;
     const {id} = jwt.verify(token,secretKey)
     try {
