c7c1d1e1b74f20dd0373219e44f1991489b07ddf
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-08-08 18:16:57 -0300

    fix ranking

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -7,7 +7,7 @@ export async function ranking(req, res){
       const {rows: ranking} = await connection.query(`
       SELECT users.id, users.name AS name, 
 count(urls.id) as linksCount, SUM(urls."visitCount") as "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" 
-GROUP BY users.id ORDER BY urls."visitCount" LIMIT 10;`)
+GROUP BY urls."visitCount"  LIMIT 10;`)
       res.status(200).send(ranking);
    } catch (error) {
     res.status(500).send(error); 
